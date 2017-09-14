6419fba44bf64544c84ba734cf5b4ef7908ce45e
Eftakhairul Islam <eftakhairul@gmail.com>
2017-09-13 23:19:49 -0400

    basic structure is added

---
+++ Gulpfule.js
@@ -0,0 +1,43 @@
+const gulp = require("gulp");
+const nodemon = require('gulp-nodemon');
+const shell = require('gulp-shell');
+
+const srcdirs = ['endpoints/', 'config/', 'lib/'];
+const watches = ['index.ts'].concat(srcdirs);
+const sources = ['index.ts'].concat(srcdirs.map(f => `${f}**/*.js`), srcdirs.map(f => `${f}**/*.ts`));
+
+
+gulp.task('typescript', function () {
+  return gulp.src('index.ts').pipe(shell('tsc -p tsconfig.json'));
+});
+
+gulp.task('typescriptSpec', () => {
+  return gulp.src('./spec/tsconfig.json').pipe(shell("tsc -p ./spec/tsconfig.json"))
+})
+
+gulp.task("copy", function () {
+	return gulp.src([ "lib/**/*.json", "config/**/*.json", "config/conf/*.js"], { base: '.' })
+               .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copySpec", function () {
+	return gulp.src([ "lib/**/*.json", "config/**/*.json", "config/conf/*.js"], { base: '.' })
+               .pipe(gulp.dest("distSpec"));
+});
+
+gulp.task("build", ["copy", "typescript"]);
+gulp.task("buildSpec", ["copySpec", "typescriptSpec"]);
+
+gulp.task('serve', ['build'], function () {
+  nodemon({
+    script: 'dist/',
+    exec: 'node --inspect',
+	watch: watches,
+    tasks: ['build'],
+    ext: 'js,ts,json',
+    env: {
+      NODE_ENV: process.env.NODE_ENV || 'dev',
+    },
+  }).on('restart', function () {
+  });
+});
