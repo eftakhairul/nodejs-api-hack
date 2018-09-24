8cdd78eff5f36c0cc524d5ed496e493450262c98
Eftakhairul Islam <eftakhairul@gmail.com>
2018-09-23 21:03:09 -0400

    http server introduced

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as http  from 'http';
 import * as express from 'express';
 import {Request, Response} from "express";
 import { App } from "lib/core/App";
@@ -21,7 +22,24 @@ router.use('/sys/health', (request: Request, response: Response) => {
 
 App.use(router);
 
+/**
+ * Setting up port
+ */
 const port: number = parseInt(process.env.NODE_PORT as string, 10) || 3000;
-App.listen(port, () => {  
-  process.stdout.write(`API server Up and Running on port ${port} \n`);
+App.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(App);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('listening', () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'? 'pipe ' + addr : 'port ' + addr.port;
+
+  process.stdout.write(`API server Up and Running on ${bind} \n`);
 });
