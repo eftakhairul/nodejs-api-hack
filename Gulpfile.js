50a81d1fa31612c71c4821f6a9d81605ca0c3618
Eftakhairul Islam <eftakhairul@gmail.com>
2018-10-21 03:23:49 -0400

    gulp is updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,17 +4,15 @@ const shell   = require('gulp-shell');
 const clean   = require('gulp-clean');
 const series  = gulp.series;
 
-const srcdirs = ['src/', 'config/'];
+const srcdirs = ['./src/', './config/'];
 const watches = ['./src/index.ts'].concat(srcdirs);
-// const sources = ['./src/index.ts'].concat(srcdirs.map(f => `${f}**/*.js`), srcdirs.map(f => `${f}**/*.ts`));
-
 
 gulp.task('typescript', function () {
-  return gulp.src('./src/index.ts').pipe(shell('tsc -p tsconfig.json'));
+  return gulp.src('./src/index.ts').pipe(shell('./node_modules/.bin/tsc -p tsconfig.json'));
 });
 
 gulp.task('typescriptSpec', () => {
-  return gulp.src('./spec/tsconfig.json').pipe(shell("tsc -p ./spec/tsconfig.json"))
+  return gulp.src('./spec/tsconfig.json').pipe(shell("./node_modules/.bin/tsc -p ./spec/tsconfig.json"))
 })
 
 gulp.task('clean', function() {
@@ -32,10 +30,7 @@ gulp.task('copySpec', function () {
              .pipe(gulp.dest("distSpec"));
 });
 
-gulp.task('build', series(['clean', 'copy', 'typescript']));
-gulp.task('buildSpec', series(['copySpec', 'typescriptSpec']));
-
-gulp.task('serve', series(['copy', 'typescript']), function () {
+gulp.task('start', function () {
   nodemon({
     script: 'build/',
     exec: 'node --inspect',
@@ -48,3 +43,7 @@ gulp.task('serve', series(['copy', 'typescript']), function () {
   }).on('restart', function () {
   });
 });
+
+gulp.task('build', series(['clean', 'copy', 'typescript']));
+gulp.task('buildSpec', series(['copySpec', 'typescriptSpec']));
+gulp.task('serve', series(['copy', 'typescript', 'start']));
